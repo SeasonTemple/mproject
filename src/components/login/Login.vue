320f84e69ae66c4f1972ff15baef877a092a8327
SeasonTemple <512743454@qq.com>
2020-02-04 15:46:18 +0800

    ...

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,9 +1,11 @@
 <template>
+  <canvas>
     <div id="parent">
-        <el-form id="userFrom">
-          
-        </el-form>
+      <el-form id="userFrom">
+
+      </el-form>
     </div>
+  </canvas>
 </template>
 
 <script>
@@ -11,8 +13,55 @@
     name: 'login',
     data() {
       let mode = 'login';
+      const STAR_COUNT = () => {
+          this.getStars()
+        },
+        STAR_SIZE = 3,
+        STAR_MIN_SCALE = 0.2,
+        OVERFLOW_THRESHOLD = 50;
+
+      const canvas = () => {
+          this.getCanvas()
+        },
+        context = () => {
+          this.getContext()
+        };
+
+      let scale = 1, // device pixel ratio
+        width,
+        height;
+
+      let stars = [];
+
+      let pointerX,
+        pointerY;
+
+      let velocity = {
+        x: 0,
+        y: 0,
+        tx: 0,
+        ty: 0,
+        z: 0.0005
+      };
+
+      let touchInput = false;
+
       return {
-        mode
+        mode,
+        STAR_COUNT,
+        STAR_SIZE,
+        STAR_MIN_SCALE,
+        OVERFLOW_THRESHOLD,
+        canvas,
+        context,
+        scale,
+        width,
+        height,
+        stars,
+        pointerX,
+        pointerY,
+        velocity,
+        touchInput
       }
     },
     methods: {
@@ -20,6 +69,9 @@
     },
     computed: {
 
+    },
+    mounted() {
+      
     }
 
   }
