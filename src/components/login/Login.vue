f6579b370333fb2be8902e1cde4f12d89ea106f0
SeasonTemple <512743454@qq.com>
2020-02-12 22:57:57 +0800

    login.vue optimized(scss,tag)

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
     <el-image :style="bgStyle" :src='url' fit="cover" :lazy='true'></el-image>
-    <div class=""></div>    
-    <div class="parent">
+    <div class="left" :class="{isSwitch: mode=='register'}">
       <el-form ref="form" :model="form">
-        <el-form-item label="请输入你的邮箱" :class="{isFocus: actFocus.isFocus}" key="1">
-          <el-input v-model="form.account" autocomplete maxlength="30" show-word-limit
+        <el-form-item label="Username" :class="{isFocus: actFocus.isFocus}">
+          <el-input v-model="form.account" autocomplete maxlength="20" show-word-limit
             @focus.stop="inputFocus(actFocus.name)" @blur.stop="inputblur(actFocus.name)" prop="username">
             <i class="el-icon-s-custom" slot="prepend"></i>
           </el-input>
         </el-form-item>
-        <el-form-item label="请输入你的密码" :class="{isFocus: pwdFocus.isFocus}" key="2">
+        <el-form-item label="Password" :class="{isFocus: pwdFocus.isFocus}">
           <el-input v-model="form.password" autocomplete show-password @focus.stop="inputFocus(pwdFocus.name)"
             @blur.stop="inputblur(pwdFocus.name)" prop="password">
             <i class="el-icon-lock" slot="prepend"></i>
           </el-input>
         </el-form-item>
+        <el-form-item :class="{}" v-if="mode=='register'">
+          <el-input v-model="form.valiCode" prop="valiCode" id="valiCode">
+            <i class="el-icon-chat-dot-round" slot="prepend"></i>
+            <el-button type="success" slot="suffix" class="valiBtn">获取验证码</el-button>
+          <!-- <el-tooltip content="获取验证码" placement="bottom" effect="light" slot="append">
+            <i class="el-icon-chat-dot-round" style="color:rgba(224, 224, 224, 0.959);font-size: 1em;font-weight: 300;cursor:pointer"></i>
+          </el-tooltip> -->
+          </el-input>
+        </el-form-item>
         <el-form-item>
-          <el-button :class="{logBtn: true}" >登&nbsp;&nbsp;&nbsp;&nbsp;录</el-button>
+          <el-button class="logBtn">登&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;录</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="regBtn" @click="switchReg">没有账号?立即注册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -42,7 +53,7 @@
       let form = {
         account: '',
         password: '',
-        valiCode: 0,
+        valiCode: '',
       };
       let actFocus = {
         name: 'account',
@@ -62,7 +73,11 @@
       }
     },
     methods: {
-
+      switchReg: function () {
+        let t = this.mode;
+        console.log(t);
+        return t == 'register' ? this.mode = 'login' : this.mode = 'register';
+      }
     },
     computed: {
       inputFocus: function () {
