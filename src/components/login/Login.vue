4a7f300e63a4583d4419e3e02847893392f5ee41
SeasonTemple <512743454@qq.com>
2020-02-18 11:23:17 +0800

    。。。。

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -10,27 +10,32 @@
           </el-input>
         </el-form-item>
         <el-form-item label="Password" :class="{isFocus: pwdFocus.isFocus}">
-          <el-input v-model="form.password" autocomplete show-password @focus.stop="inputFocus(pwdFocus.name)"
-            @blur.stop="inputblur(pwdFocus.name)" prop="password">
+          <el-input v-model="form.password" autocomplete maxlength="20" show-password
+            @focus.stop="inputFocus(pwdFocus.name)" @blur.stop="inputblur(pwdFocus.name)" prop="password">
             <i class="el-icon-lock" slot="prepend"></i>
           </el-input>
         </el-form-item>
-        <el-form-item :class="{}" v-if="mode=='register'">
-          <el-input v-model="form.valiCode" prop="valiCode" id="valiCode">
+        <el-form-item v-if="mode!='usePassword'">
+          <el-input v-model="form.valiCode" prop="valiCode" id="valiCode" maxlength="6">
             <i class="el-icon-chat-dot-round" slot="prepend"></i>
             <el-button type="success" slot="suffix" class="valiBtn">获取验证码</el-button>
-          <!-- <el-tooltip content="获取验证码" placement="bottom" effect="light" slot="append">
+            <!-- <el-tooltip content="获取验证码" placement="bottom" effect="light" slot="append">
             <i class="el-icon-chat-dot-round" style="color:rgba(224, 224, 224, 0.959);font-size: 1em;font-weight: 300;cursor:pointer"></i>
           </el-tooltip> -->
           </el-input>
         </el-form-item>
+          <el-link class="forget">忘记密码?</el-link>
         <el-form-item>
           <el-button class="logBtn">登&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;录</el-button>
         </el-form-item>
-        <el-form-item>
+        <el-form-item >
+          <!-- v-if="mode!='register'" -->
           <el-button class="regBtn" @click="switchReg">没有账号?立即注册</el-button>
         </el-form-item>
       </el-form>
+    </div>
+    <div class="goRegister">
+      点击前往注册
     </div>
   </div>
 </template>
@@ -46,6 +51,7 @@
       let bgStyle = {
         height: '100%',
         width: '100%',
+        color: 'rgba(224, 224, 224, 0.959)',
         backgroundRepeat: 'no-repeat',
         position: 'absolute',
         zIndex: -1
