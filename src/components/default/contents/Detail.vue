0706b5f5dc260c2b2049ae3a8ec5c4918276f000
SeasonTemple <512743454@qq.com>
2020-03-09 22:47:53 +0800

    detail.vue -> progress import

--- src/components/default/contents/Detail.vue
+++ src/components/default/contents/Detail.vue
@@ -1,106 +1,144 @@
 <template>
-  <div class="box">
-    <el-form ref="detailForm" :model="detailForm" label-width="auto" class="detailStyle">
-      <el-form-item class="user-avatar " label="我的头像">
-        <el-upload class="avatar-uploader" ref="upload" action="https://jsonplaceholder.typicode.com/posts/"
-          :before-upload="beforeAvatarUpload" :on-preview="handlePictureCardPreview" list-type="picture-card"
-          :on-progress="handleUpload" :on-remove="handleRemove" :on-success="handleAvatarSuccess">
-          <i class="el-icon-plus avatar-uploader-icon"></i>
-        </el-upload>
-        <el-dialog :visible.sync="dialogVisible" :append-to-body='true'>
-          <img width="100%" :src="detailForm.imageUrl" alt="头像">
-        </el-dialog>
-      </el-form-item>
-      <el-form-item label="用户名">
-        <el-input v-model="detailForm.username" minlength="8" maxlength="20" show-word-limit placeholder="请输入你的用户名"
-          clearable>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="detailForm.password" minlength="8" maxlength="20" show-password placeholder="请输入你的密码"
-          clearable></el-input>
-      </el-form-item>
-      <el-form-item label="真实姓名">
-        <el-input v-model="detailForm.realName" minlength="2" maxlength="10" style="width:60%;" placeholder="请输入你的真实姓名"
-          clearable></el-input>
-      </el-form-item>
-      <el-form-item label="手机号">
-        <el-input v-model="detailForm.phone" maxlength="11" show-word-limit style="width:60%;" placeholder="请输入你的手机号"
-          clearable>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="性别">
-        <el-radio-group v-model="detailForm.sex" text-color="#F2F6FC" fill="#67C23A">
-          <el-radio-button label="男"></el-radio-button>
-          <el-radio-button label="女"></el-radio-button>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="年龄">
-        <el-input-number v-model="detailForm.age" :min="1" :max="100" size="medium" placeholder="输入或按钮调节">
-        </el-input-number>
-      </el-form-item>
-      <el-form-item label="职位">
-        <el-input v-model="detailForm.position" maxlength="20" placeholder="请输入你的职位名称" style="width:72%;" clearable>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="身份证号">
-        <el-input v-model="detailForm.idNum" maxlength="18" show-word-limit style="width:72%;" placeholder="请输入你的身份证号码"
-          clearable></el-input>
-      </el-form-item>
-      <el-form-item label="职工号">
-        <el-input v-model="detailForm.empId" maxlength="18" show-word-limit style="width:72%;" placeholder="请输入你的职工编号"
-          clearable>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="所属项目组">
-        <el-input v-model="detailForm.groupName" maxlength="10" placeholder="请输入你所属的项目组名称" style="width:72%" readonly
-          clearable>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="所属部门">
-        <el-input v-model="detailForm.departmentName" maxlength="10" placeholder="请输入你所属的部门名称" style="width:72%"
-          readonly clearable></el-input>
-      </el-form-item>
-      <el-form-item label="直属上级">
-        <el-input v-model='detailForm.leaderName' minlength="2" maxlength="10" placeholder="请输入你的直属上级全名"
-          style="width:72%" readonly clearable></el-input>
-      </el-form-item>
-      <el-form-item label="入职日期">
-        <el-date-picker v-model="detailForm.createTime" value-format="yyyy-MM-dd HH:mm:ss" type="datetime"
-          :picker-options="pickerOptions" readonly clearable>
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="上次登录时间">
-        <el-date-picker v-model="detailForm.lastLogin" value-format="yyyy-MM-dd HH:mm:ss" type="datetime"
-          :picker-options="pickerOptions" readonly clearable>
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary">提交修改</el-button>
-        <el-button @click="resetForm('detailForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+  <el-row class="box" :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+    <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+      <el-form ref="detailForm" :model="detailForm" label-width="auto" class="detailStyle">
+        <el-form-item class="user-avatar " label="我的头像" prop="imageUrl">
+          <el-upload class="avatar-uploader" ref="upload" action="https://jsonplaceholder.typicode.com/posts/"
+            :before-upload="beforeAvatarUpload" :on-preview="handlePictureCardPreview" list-type="picture-card"
+            :on-progress="handleUpload" :on-remove="handleRemove" :on-success="handleAvatarSuccess">
+            <i class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible" :append-to-body='true'>
+            <img width="100%" :src="detailForm.imageUrl" alt="你的头像">
+          </el-dialog>
+        </el-form-item>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="detailForm.username" minlength="8" maxlength="20" show-word-limit :style="formItemWidth"
+            placeholder="填写你的用户名" v-bind="formState">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="detailForm.password" minlength="8" maxlength="20" show-password :style="formItemWidth"
+            placeholder="填写你的密码" v-bind="formState"></el-input>
+        </el-form-item>
+        <el-form-item label="真实姓名" prop="realName">
+          <el-input v-model="detailForm.realName" minlength="2" maxlength="10" :style="formItemWidth" show-word-limit
+            placeholder="填写你的真实姓名" v-bind="formState">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="phone">
+          <el-input v-model="detailForm.phone" maxlength="11" show-word-limit :style="formItemWidth"
+            placeholder="填写你的手机号" v-bind="formState">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="性别" prop="sex">
+          <el-radio-group v-model="detailForm.sex" text-color="#F2F6FC" fill="#67C23A" size="medium" v-bind="formState">
+            <el-radio-button label="1">男</el-radio-button>
+            <el-radio-button label="0">女</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input-number v-model="detailForm.age" :min="1" :max="100" size="small" controls-position="right"
+            placeholder="你的年龄" v-bind="formState">
+          </el-input-number>
+        </el-form-item>
+        <!-- <el-form-item label="职位">
+        <el-input v-model="detailForm.position" maxlength="25" placeholder="请输入你的职位名称" :style="formItemWidth" clearable>
+        </el-input> -->
+        <!-- </el-form-item> -->
+        <el-form-item label="身份证号" prop="idNum">
+          <el-input v-model="detailForm.idNum" maxlength="18" show-word-limit :style="formItemWidth"
+            placeholder="填写你的身份证号码" v-bind="formState"></el-input>
+        </el-form-item>
+        <el-form-item label="职工号" prop="empId">
+          <el-input v-model="detailForm.empId" maxlength="18" show-word-limit :style="formItemWidth"
+            placeholder="填写你的职工编号" v-bind="formState">
+          </el-input>
+        </el-form-item>
+        <el-form-item label="所属" prop="belongTo">
+          <el-cascader v-model="detailForm.belongTo" :options="belongOpts" :props="{ expandTrigger: 'hover' }"
+            :style="formItemWidth" v-bind="formState">
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="直属上级" prop="leader">
+          <el-input v-model='detailForm.leaderName' minlength="2" maxlength="10" placeholder="你的直属上级姓名"
+            :style="formItemWidth" v-bind="formState"></el-input>
+        </el-form-item>
+        <el-form-item label="入职日期" prop="createTime">
+          <el-date-picker v-model="detailForm.createTime" value-format="yyyy-MM-dd HH:mm:ss" type="datetime"
+            :picker-options="pickerOptions" :style="formItemWidth" v-bind="formState">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="上次登录" prop="lastLogin">
+          <el-date-picker v-model="detailForm.lastLogin" value-format="yyyy-MM-dd HH:mm:ss" type="datetime"
+            :picker-options="pickerOptions" :style="formItemWidth" v-bind="formState">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">提交修改</el-button>
+          <el-button @click="resetForm('detailForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+    <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
+      <template v-for="(p, index) in progressGroup.progress">
+        <!-- <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" :class="{circleGroup:true}"> -->
+        <el-progress v-if="p.type == 'line'" type="p.type" :percentage="p.percentage" :text-inside="p.textInside" :color="progressGroup.colors" :key="index" :status="p.status" :stroke-width="p.strokeWidth" :show-text="p.showText" ></el-progress>
+        <el-progress v-else type="p.type" :percentage="p.percentage" :color="progressGroup.colors" :key="index" :status="p.status" :stroke-width="p.strokeWidth" :width="p.width" :show-text="p.showText" :stroke-linecap="p.strokeLineCap"></el-progress>
+      </template>
+    </el-col>
+  </el-row>
 </template>
 <script>
   export default {
     name: 'profile',
     data() {
+      let formItemWidth = 'width:70%';
       let dialogVisible = false;
       let disabled = false;
+      let formState = {
+        readonly: false,
+        clearable: true,
+        disabled: false
+      };
+      let belongOpts = [{
+        label: '指南',
+        departmentName: 'zhinan',
+        departmentId: '1',
+        children: [{
+          label: '设计原则',
+          groupName: 'shejiyuanze',
+          groupId: '2',
+          children: [{
+            label: '一致',
+            position: 'shejiyuanze',
+            positionId: '2',
+          }]
+        }, {
+          label: '设计原则',
+          groupName: 'shejiyuanze',
+          groupId: '2',
+          children: [{
+            label: '一致',
+            position: 'shejiyuanze',
+            positionId: '2',
+          }]
+        }]
+      }];
       let detailForm = {
         imageUrl: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
         username: '廖某人',
         password: '.z54564',
         realName: '廖文岵',
         phone: '110',
-        sex: '男',
-        age: '21',
+        sex: '1',
+        age: 21,
         position: 'Python删库员',
         idNum: '51025198704013312',
         empId: '192382149870231001',
         createTime: new Date(),
         groupName: '大国中医',
+        belongTo: [],
         departmentName: '传销部',
         leaderName: '习近平',
         lastLogin: new Date()
@@ -130,11 +168,57 @@
           }
         }]
       };
+      let progressGroup = {
+        progress: [{
+          type: 'circle',
+          percentage: 12,
+          strokeWidth: 6,
+          textInside: false,
+          status: 'success',
+          width: 150,
+          showText: false,
+          strokeLineCap: 'round'
+        },{
+          type: 'circle',
+          percentage: 12,
+          strokeWidth: 6,
+          textInside: false,
+          status: 'success',
+          width: 150,
+          showText: false,
+          strokeLinecap: 'round'
+        }],
+        colors: [{
+            color: '#f56c6c',
+            percentage: 20
+          },
+          {
+            color: '#e6a23c',
+            percentage: 40
+          },
+          {
+            color: '#5cb87a',
+            percentage: 60
+          },
+          {
+            color: '#1989fa',
+            percentage: 80
+          },
+          {
+            color: '#6f7ad3',
+            percentage: 100
+          }
+        ]
+      };
       return {
         dialogVisible,
         disabled,
         detailForm,
-        pickerOptions
+        formItemWidth,
+        pickerOptions,
+        belongOpts,
+        formState,
+        progressGroup
       }
     },
     methods: {
